4b4db34e24595fc488c201a7f476f5773b5df04e
Jean-Rémy <ayL47@users.noreply.github.com>
2014-05-10 14:10:47 +0200

    Menu: ajout boutons retour

--- c1/testsdl/main.c
+++ c1/testsdl/main.c
@@ -35,12 +35,15 @@ void menu()
     SDL_Surface *buttonRank = NULL;
     SDL_Surface *rank = NULL;
     SDL_Surface *buttonQuit = NULL;
+    SDL_Surface *buttonBack = NULL;
+    SDL_Surface *menu = NULL;
 
     SDL_Rect positionButtonPlay;
     SDL_Rect positionButtonIns;
     SDL_Rect position;
     SDL_Rect positionButtonRank;
     SDL_Rect positionButtonQ;
+    SDL_Rect positionButtonBack;
 
     buttonPlay = SDL_LoadBMP("boutonJ_pas_survole.bmp");
     buttonIns = SDL_LoadBMP("boutonIns_pas_survole.bmp");
@@ -48,6 +51,8 @@ void menu()
     buttonRank = SDL_LoadBMP("boutonS_pas_survole.bmp");
     rank = SDL_LoadBMP("Scores.bmp");
     buttonQuit = SDL_LoadBMP("boutonQ_pas_survole.bmp");
+    buttonBack = SDL_LoadBMP("boutonR_pas_survole.bmp");
+    menu = SDL_LoadBMP("menu.bmp");
 
 
     positionButtonPlay.x = 125;
@@ -60,7 +65,10 @@ void menu()
     positionButtonRank.y = positionButtonIns.y+40;
     positionButtonQ.x = positionButtonPlay.x;
     positionButtonQ.y = positionButtonRank.y+40;
+    positionButtonBack.x = 0;
+    positionButtonBack.y = 563;
 
+    SDL_BlitSurface(menu, NULL, SDL_GetVideoSurface(), &position);
     SDL_BlitSurface(buttonPlay, NULL, SDL_GetVideoSurface(), &positionButtonPlay);
     SDL_BlitSurface(buttonIns, NULL, SDL_GetVideoSurface(), &positionButtonIns);
     SDL_BlitSurface(buttonRank, NULL, SDL_GetVideoSurface(), &positionButtonRank);
@@ -70,40 +78,39 @@ void menu()
 
     while(continuer)
     {
-                SDL_WaitEvent(&event); /// attente d'un event
+                SDL_WaitEvent(&event);
         switch(event.type)
         {
             case SDL_MOUSEBUTTONUP:
                 if (event.button.x>=positionButtonPlay.x && event.button.x<=positionButtonPlay.x+233 && event.button.y>=positionButtonPlay.y && event.button.y<=positionButtonPlay.y+37)
                 {
-                    play();
+                    play();/*si souris sur bouton jouer*/
                 }
                 if (event.button.x>=positionButtonIns.x && event.button.x<=positionButtonIns.x+233 && event.button.y>=positionButtonIns.y && event.button.y<=positionButtonIns.y+37)
                 {
-                    SDL_BlitSurface(instructions, NULL, SDL_GetVideoSurface(), &position);
-                    SDL_Flip(SDL_GetVideoSurface());
+                    instruction(instructions, position, buttonBack, positionButtonBack);/*si souris sur bouton instructions*/
+
                 }
                 if (event.button.x>=positionButtonRank.x && event.button.x<=positionButtonRank.x+233 && event.button.y>=positionButtonRank.y && event.button.y<=positionButtonRank.y+37)
                 {
-                    SDL_BlitSurface(rank, NULL, SDL_GetVideoSurface(), &position);
-                    SDL_Flip(SDL_GetVideoSurface());
+                    score(rank, position, buttonBack, positionButtonBack);
                 }
                 if (event.button.x>=positionButtonQ.x && event.button.x<=positionButtonQ.x+233 && event.button.y>=positionButtonQ.y && event.button.y<=positionButtonQ.y+37)
                 {
-                    close();
+                    close();/*si souris sur bouton quitter*/
                 }
                 else
                 {
                     continuer = 1;
                 }
                 break;
-            case SDL_QUIT: /// si type = SDL_QUIT
+            case SDL_QUIT:
                 continuer = 0;
                 break;
-            case SDL_KEYDOWN: /// si type = TOUCHE APPUYE
+            case SDL_KEYDOWN:
                 switch (event.key.keysym.sym)
                 {
-                    case SDLK_ESCAPE: /// si touche = ECHAP
+                    case SDLK_ESCAPE:
                         continuer = 0;
                         break;
                 }
@@ -115,10 +122,86 @@ void menu()
 
 
 
-
     SDL_FreeSurface(buttonPlay);
+    SDL_FreeSurface(buttonQuit);
+    SDL_FreeSurface(buttonIns);
+    SDL_FreeSurface(buttonRank);
+    SDL_FreeSurface(instructions);
+    SDL_FreeSurface(rank);
+    SDL_FreeSurface(buttonBack);
 }
 
+void instruction(SDL_Surface *instructions, SDL_Rect position, SDL_Surface *buttonBack, SDL_Rect positionButtonBack)
+{
+    int continuer = 1;
+    SDL_Event event;
+
+    SDL_BlitSurface(instructions, NULL, SDL_GetVideoSurface(), &position);
+    SDL_BlitSurface(buttonBack, NULL, SDL_GetVideoSurface(), &positionButtonBack);
+    SDL_Flip(SDL_GetVideoSurface());
+
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_MOUSEBUTTONUP:
+                if(event.button.x>=positionButtonBack.x && event.button.x<=positionButtonBack.x+233 && event.button.y>=positionButtonBack.y && event.button.y<=positionButtonBack.y+37)
+                {
+                   menu() ;/*si souris sur bouton retour*/
+                }
+            break;
+            case SDL_QUIT:
+                continuer = 0;
+                break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE:
+                        continuer = 0;
+                        break;
+                }
+
+            break;
+        }
+    }
+}
+
+void score(SDL_Surface *rank, SDL_Rect position, SDL_Surface *buttonBack, SDL_Rect positionButtonBack)
+{
+    int continuer = 1;
+    SDL_Event event;
+
+    SDL_BlitSurface(rank, NULL, SDL_GetVideoSurface(), &position);
+    SDL_BlitSurface(buttonBack, NULL, SDL_GetVideoSurface(), &positionButtonBack);
+    SDL_Flip(SDL_GetVideoSurface());
+
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_MOUSEBUTTONUP:
+                if(event.button.x>=positionButtonBack.x && event.button.x<=positionButtonBack.x+233 && event.button.y>=positionButtonBack.y && event.button.y<=positionButtonBack.y+37)
+                {
+                   menu() ;/*si souris sur bouton retour*/
+                }
+            break;
+            case SDL_QUIT:
+                continuer = 0;
+                break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE:
+                        continuer = 0;
+                        break;
+                }
+
+            break;
+        }
+    }
+}
 
 /*int main(int argc, char *argv[])
 {
