30d5a680a4ed265f1c3707f4c57955610688e4a2
Jean-Rémy <ayL47@users.noreply.github.com>
2014-05-10 12:44:50 +0200

    Creation d'un menu 'jouer/instructions'

--- c1/testsdl/main.c
+++ c1/testsdl/main.c
@@ -10,8 +10,8 @@ int main(int argc, char *argv[])
     SDL_Init(SDL_INIT_VIDEO);
     SDL_SetVideoMode(LARGEUR_FENETRE, HAUTEUR_FENETRE, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
     SDL_WM_SetCaption(":: Casse brique ::", NULL);
-    /*menu();*/
-    play();
+    menu();
+    /*play();*/
     close();
 
     return 0;
@@ -22,6 +22,81 @@ void close(void)
 
     SDL_Quit(); /* Fermeture SDL */
 
+}
+
+void menu()
+{
+    int continuer = 1;
+    SDL_Event event;
+
+    SDL_Surface *buttonPlay = NULL;
+    SDL_Surface *buttonIns = NULL;
+    SDL_Surface *instructions = NULL;
+    SDL_Rect positionButtonPlay;
+    SDL_Rect positionButtonIns;
+    SDL_Rect positionInstruction;
+
+    buttonPlay = SDL_LoadBMP("boutonJ_pas_survole.bmp");
+    buttonIns = SDL_LoadBMP("boutonIns_pas_survole.bmp");
+    instructions = SDL_LoadBMP("Instructions.bmp");
+
+
+    positionButtonPlay.x=125;
+    positionButtonPlay.y=200;
+    positionButtonIns.x=125;
+    positionButtonIns.y=240;
+    positionInstruction.x=0;
+    positionInstruction.y=0;
+
+    SDL_BlitSurface(buttonPlay, NULL, SDL_GetVideoSurface(), &positionButtonPlay);
+    SDL_BlitSurface(buttonIns, NULL, SDL_GetVideoSurface(), &positionButtonIns);
+
+    SDL_Flip(SDL_GetVideoSurface());
+
+    while(continuer)
+    {
+                SDL_WaitEvent(&event); /// attente d'un event
+        switch(event.type)
+        {
+            case SDL_MOUSEBUTTONUP:
+                if (event.button.x>=positionButtonPlay.x && event.button.x<=positionButtonPlay.x+233 && event.button.y>=positionButtonPlay.y && event.button.y<=positionButtonPlay.y+37)
+                {
+                    play();
+                }
+                if (event.button.x>=positionButtonIns.x && event.button.x<=positionButtonIns.x+233 && event.button.y>=positionButtonIns.y && event.button.y<=positionButtonIns.y+37)
+                {
+                    SDL_BlitSurface(instructions, NULL, SDL_GetVideoSurface(), &positionInstruction);
+                    SDL_Flip(SDL_GetVideoSurface());
+                }
+                /*if (event.button.x>=posCredits.x && event.button.x<=posCredits.x+LONGUEUR && event.button.y>=posCredits.y && event.button.y<=posCredits.y+LARGEUR)
+                {
+                    credits(ecran);
+                }*/
+                else
+                {
+                    continuer = 1;
+                }
+                break;
+            case SDL_QUIT: /// si type = SDL_QUIT
+                continuer = 0;
+                break;
+            case SDL_KEYDOWN: /// si type = TOUCHE APPUYE
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE: /// si touche = ECHAP
+                        continuer = 0;
+                        break;
+                }
+
+            break;
+        }
+    }
+
+
+
+
+
+    SDL_FreeSurface(buttonPlay);
 }
 
 
