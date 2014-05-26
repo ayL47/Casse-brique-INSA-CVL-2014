320dd1ad0f9debf3a682e5e0c70785c8c056e627
Jean-Rémy <ayL47@users.noreply.github.com>
2014-05-26 23:37:42 +0200

    Correction bug entrée pseudo du joueur

--- CasseBrique/classement.c
+++ CasseBrique/classement.c
@@ -10,36 +10,31 @@
 
 void saisieTexte(Joueur *joueur){
 
-    SDL_Event eventSaisie; // tu crée une variable d'evenements .
-    SDL_EnableUNICODE(1);  // Cette ligne permet d'activer l'UNICODE, c'est a dire que les caracteres speciaux sont acceptés
+    SDL_Event eventSaisie;
+    SDL_EnableUNICODE(1);
 
-    int position = 0, continuerSaisie = 1; // pour continuer la saisie et position permet de se repéré dans le tableau (pour pas supprimer une ancienne entrée)
+    int position = 0, continuerSaisie = 1;
     int nb = 0;
-    while(continuerSaisie) // La boucle (pour que des que le joueur appuis sur le clavier, il se passe un evenement
-    {
+    while(continuerSaisie) {
         SDL_WaitEvent(&eventSaisie);
-        if(eventSaisie.type == SDL_KEYDOWN)
-        {
-            if(eventSaisie.key.keysym.sym == SDLK_BACKSPACE) // Si il appuis sur la touche retour arriere...
-            {
-                position--; // ... on  recule le curseur de 1 (comme un editeur de texte)
-                joueur->pseudo[position] = '\0'; // Efface l'ancienne saisie
+        if(eventSaisie.type == SDL_KEYDOWN){
+            if(eventSaisie.key.keysym.sym == SDLK_RETURN){
+                SDL_EnableUNICODE(0);
+                continuerSaisie = 0;
             }
-            if(eventSaisie.key.keysym.sym == SDLK_RETURN)
-            {
-                SDL_EnableUNICODE(0); // Si il appuis sur entrée , on desactive l'UNICODE
-                continuerSaisie = 0; // On quitte la boucle
+            else if(eventSaisie.key.keysym.sym == SDLK_SPACE){
+                SDL_EnableUNICODE(0);
+                continuerSaisie = 0;
             }
-            if(((eventSaisie.key.keysym.unicode & 0xFF00) == 0x0000) && (position + 1 < TAILLE_MAX_PSEUDO) ) // ça je n'est pas trop compris, mais le deuxieme argument et que si le curseur n'est pas a la fin du tableau ...
-            {
-                joueur->pseudo[position] = eventSaisie.key.keysym.unicode; // on enregistre la touche appuyée dans le tableau à la position du curseur
-                position++; // on avance le curseur de 1 (pour évité de supprimé l'ancienne entrée de l'utilisateur
-                joueur->pseudo[position] = '\0'; // ...et on vide cette' nouvelle case pour la prochaine entrée.
+            else if(((eventSaisie.key.keysym.unicode & 0xFF00) == 0x0000) && (position + 1 < TAILLE_MAX_PSEUDO) ) {
+                joueur->pseudo[position] = eventSaisie.key.keysym.unicode;
+                position++;
+                joueur->pseudo[position] = '\0';
                 nb++;
                 afficheTexte(*joueur, nb);
             }
         }
-        SDL_Flip(SDL_GetVideoSurface());
+    SDL_Flip(SDL_GetVideoSurface());
     }
 }
 
@@ -66,8 +61,8 @@ void afficheImgSaisie (int life, int score){
 
 void afficheTexte (Joueur joueur, int nb){
 
-    int i = 0, j = 0, b =0;
-    b = joueur.pseudo[nb -1] - 97;
+    int i = 0, b =0;
+    b = joueur.pseudo[nb -1] - 97; // 97 est le code de 'a'
 
     SDL_Rect position;
 
@@ -77,11 +72,35 @@ void afficheTexte (Joueur joueur, int nb){
     SDL_Surface **imglettre;
     imglettre = (SDL_Surface**)malloc(sizeof(SDL_Surface*)*26);
 
-    imglettre[0] = SDL_LoadBMP("images/a.bmp");
-    imglettre[1] = SDL_LoadBMP("images/b.bmp");
-    imglettre[2] = SDL_LoadBMP("images/c.bmp");
-
+    imglettre[0] = SDL_LoadBMP("images/lettres/a.bmp");
+    imglettre[1] = SDL_LoadBMP("images/lettres/b.bmp");
+    imglettre[2] = SDL_LoadBMP("images/lettres/c.bmp");
+    imglettre[3] = SDL_LoadBMP("images/lettres/d.bmp");
+    imglettre[4] = SDL_LoadBMP("images/lettres/e.bmp");
+    imglettre[5] = SDL_LoadBMP("images/lettres/f.bmp");
+    imglettre[6] = SDL_LoadBMP("images/lettres/g.bmp");
+    imglettre[7] = SDL_LoadBMP("images/lettres/h.bmp");
+    imglettre[8] = SDL_LoadBMP("images/lettres/i.bmp");
+    imglettre[9] = SDL_LoadBMP("images/lettres/j.bmp");
+    imglettre[10] = SDL_LoadBMP("images/lettres/k.bmp");
+    imglettre[11] = SDL_LoadBMP("images/lettres/l.bmp");
+    imglettre[12] = SDL_LoadBMP("images/lettres/m.bmp");
+    imglettre[13] = SDL_LoadBMP("images/lettres/n.bmp");
+    imglettre[14] = SDL_LoadBMP("images/lettres/o.bmp");
+    imglettre[15] = SDL_LoadBMP("images/lettres/p.bmp");
+    imglettre[16] = SDL_LoadBMP("images/lettres/q.bmp");
+    imglettre[17] = SDL_LoadBMP("images/lettres/r.bmp");
+    imglettre[18] = SDL_LoadBMP("images/lettres/s.bmp");
+    imglettre[19] = SDL_LoadBMP("images/lettres/t.bmp");
+    imglettre[20] = SDL_LoadBMP("images/lettres/u.bmp");
+    imglettre[21] = SDL_LoadBMP("images/lettres/v.bmp");
+    imglettre[22] = SDL_LoadBMP("images/lettres/w.bmp");
+    imglettre[23] = SDL_LoadBMP("images/lettres/x.bmp");
+    imglettre[24] = SDL_LoadBMP("images/lettres/y.bmp");
+    imglettre[25] = SDL_LoadBMP("images/lettres/z.bmp");
 
     SDL_BlitSurface(imglettre[b], NULL, SDL_GetVideoSurface(), &position);
 
 }
+
+
